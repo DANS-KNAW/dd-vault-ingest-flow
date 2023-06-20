c673f45cdc5e314f6505c1fe860f5fcca133ee5b
Eric de Vries <hro.mail@gmail.com>
2023-06-20 11:25:20 +0200

    Remove test country resolver

--- src/test/java/nl/knaw/dans/vaultingest/core/deposit/SimpleCommonDepositManager.java
+++ src/test/java/nl/knaw/dans/vaultingest/core/deposit/SimpleCommonDepositManager.java
@@ -17,9 +17,7 @@ package nl.knaw.dans.vaultingest.core.deposit;
 
 import lombok.extern.slf4j.Slf4j;
 import nl.knaw.dans.vaultingest.core.domain.Deposit;
-import nl.knaw.dans.vaultingest.core.utilities.TestCountryResolver;
 import nl.knaw.dans.vaultingest.core.utilities.TestLanguageResolver;
-import nl.knaw.dans.vaultingest.core.validator.InvalidDepositException;
 import nl.knaw.dans.vaultingest.core.xml.XmlReaderImpl;
 
 import java.io.IOException;
@@ -27,11 +25,13 @@ import java.nio.file.Path;
 
 @Slf4j
 public class SimpleCommonDepositManager extends CommonDepositManager {
-    public SimpleCommonDepositManager() throws IOException {
+
+
+    public SimpleCommonDepositManager(CountryResolver countryResolver) throws IOException {
         super(
             new XmlReaderImpl(),
             new TestLanguageResolver(),
-            new TestCountryResolver()
+            countryResolver
         );
     }
 
