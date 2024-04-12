68b139f3a0615d8a6ea1fa8db42f4fdffec68a6f
Jan van Mansum <janvanmansum@gmail.com>
2024-04-12 11:12:57 +0200

    Do not translate sword token to urn:uuid before calling Vault Catalog, anymore.

--- src/main/java/nl/knaw/dans/vaultingest/client/VaultCatalogClientImpl.java
+++ src/main/java/nl/knaw/dans/vaultingest/client/VaultCatalogClientImpl.java
@@ -43,7 +43,7 @@ public class VaultCatalogClientImpl implements VaultCatalogClient {
         var datasetDto = new DatasetDto()
             .nbn(deposit.getNbn())
             .datastation("VaaS") // TODO: get from configuration or set in dd-transfer-to-vault (but in that case it must not be a required field)
-            .swordToken(swordTokenToUrnUuid(deposit.getSwordToken()))
+            .swordToken(deposit.getSwordToken())
             .dataSupplier(deposit.getDataSupplier())
             .addVersionExportsItem(versionExportDto);
 
@@ -54,10 +54,6 @@ public class VaultCatalogClientImpl implements VaultCatalogClient {
         catch (ApiException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private String swordTokenToUrnUuid(String swordToken) {
-        return "urn:uuid:" + swordToken.substring(swordToken.lastIndexOf(':') + 1);
     }
 
     @Override
